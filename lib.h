dd75cbb7a146fa9076921ca82bdbf357c74af8fd
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-06-06 23:06:04 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
+void init (char mat[], int n);
+void init(char mat[5][15]);
+void inserimento(char nomi[5][15], char cognomi[5][15]);
+void inserimento (char mat[], int n);
+int ricerca (char nomi[5][15], char cognomi [5][15], char nome[15], char cognome[15]);
 
